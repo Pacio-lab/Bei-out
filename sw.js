2278971fb9a43d8a8dd957ddf9301b8464054875
marketing-lab <pacmanbs84@gmail.com>
2026-08-25 19:38:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
+const CACHE_NAME = 'brixiaout-v2';
+const assetsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Installazione Service Worker e salvataggio in cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('brixiaout-store').then((cache) => {
-      return cache.addAll([
-        './index.html',
-        './manifest.json'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
     })
   );
+  self.skipWaiting();
 });
 
+// Attivazione e pulizia vecchie cache
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+// Intercettazione richieste di rete
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
